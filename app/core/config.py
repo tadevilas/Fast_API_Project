23e9c5a2cc56877d743f84abf7fe69db4c68119a
Vilas Tade <vilas.tade@ibm.com>
2026-06-09 10:29:38 +0530

    Added settings

---
+++ app/core/config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Settings:
+    PROJECT_NAME: str = "Car Price API"
+    API_KEY = os.getenv("API_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_ALGORITHM = "HS256"
+    REDIS_URL = os.getenv("REDIS_URL")
+    MODEL_PATH = "app/model/model.pkl"
+    
+    
+settings = Settings()
---
+++ app/core/security.py
@@ -0,0 +1,20 @@
+from datatime import datetime, timedelta, timezone
+from jose import JWTError, jwt
+from app.core.config import settings
+
+
+
+def create_access(data: dict, expire_minutes = 30):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=expire_minutes)
+    to_encode.update({"exp": expire})
+    
+    return jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
+
+
+def verify_access(token: str):
+    try:
+        payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
+        return payload
+    except JWTError:
+        return None
